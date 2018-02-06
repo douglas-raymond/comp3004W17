90ab03b57cc70083913a49c414b5c9aec208265e
douglas-raymond <douglas.raymond@hotmail.com>
2018-02-05 19:59:25 -0500

    Added MainMenu UI Object
    Created a game object to better handle main menu UI

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,16 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Button NewGameBtn, OptionsBtn, ExitGameBtn;
+	public GameObject PanelOptions; 
+
+	public void NewGameBtnOnClick(){
+		Debug.Log ("New Game");
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OptionsBtnOnClick(){
+		PanelOptions.SetActive (true);
+		Debug.Log ("Options");
+	}
+
+	public void ExitGameBtnOnClick(){
+		Debug.Log ("Exit Game");
+	}
+
+	public void OptionsExitBtnOnClick(){
+		PanelOptions.SetActive (false);
+		Debug.Log ("Close Options");
 	}
 }
