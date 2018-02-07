ee29ab7e59fdfff5b7c77536db8c70eadd248787
gabesinhorin <gabsinhorin@hotmail.com>
2018-02-06 20:52:56 -0500

    Add files via upload

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,28 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
 
-	public Button NewGameBtn, OptionsBtn, ExitGameBtn;
-	public GameObject PanelOptions; 
-
-	public void NewGameBtnOnClick(){
-		Debug.Log ("New Game");
+	// Use this for initialization
+	void Start () {
+		
 	}
-
-	public void OptionsBtnOnClick(){
-		PanelOptions.SetActive (true);
-		Debug.Log ("Options");
-	}
-
-	public void ExitGameBtnOnClick(){
-		Debug.Log ("Exit Game");
-	}
-
-	public void OptionsExitBtnOnClick(){
-		PanelOptions.SetActive (false);
-		Debug.Log ("Close Options");
+	
+	// Update is called once per frame
+	void Update () {
+		
 	}
 }
