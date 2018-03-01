56e2885ca41d20d9e6462e03c348d66da80b3d74
douglas-raymond <douglas.raymond@hotmail.com>
2018-02-28 23:55:44 -0500

    Update MainMenuUI.cs

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,16 +1,63 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	public Button NewGameBtn, OptionsBtn, ExitGameBtn;
+	public Dropdown DropdownAI, DropdownHuman, DropdownTestScenario;
+	public GameObject PanelOptions; 
+	int ai_DropdownVal,human_DropdownVal,test_DropdownVal;
+
+	public void NewGameBtnOnClick(){
+		Debug.Log ("New Game");
+		PlayerPrefs.SetInt("aiPlayersNum",ai_DropdownVal);
+		PlayerPrefs.SetInt ("humanPlayersNum", human_DropdownVal);
+		PlayerPrefs.SetInt ("testScenario", test_DropdownVal);
+		SceneManager.LoadScene ("inGame");
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void OptionsBtnOnClick(){
+		PanelOptions.SetActive (true);
+		Debug.Log ("Options");
 	}
+
+
+	public void ExitGameBtnOnClick(){
+		Debug.Log ("Exit Game");
+	}
+
+	public void OptionsExitBtnOnClick ()
+	{
+		PanelOptions.SetActive (false);
+		Debug.Log ("Close Options");
+	}
+
+	void Start()
+	{
+		
+		if (ai_DropdownVal == null) {
+			DropdownAI = PanelOptions.GetComponent<Dropdown> ();
+		}		
+		if (human_DropdownVal == null) {
+			DropdownHuman = PanelOptions.GetComponent<Dropdown> ();
+		}
+		if (test_DropdownVal == null) {
+			DropdownTestScenario = PanelOptions.GetComponent<Dropdown> ();		
+		}
+	}
+
+	void Update()
+	{
+		if(DropdownAI!=null)
+		ai_DropdownVal = DropdownAI.value;
+		if(DropdownHuman!=null)
+		human_DropdownVal = DropdownHuman.value;
+		if (DropdownTestScenario != null)
+		test_DropdownVal = DropdownTestScenario.value;
+	}
+
 }
