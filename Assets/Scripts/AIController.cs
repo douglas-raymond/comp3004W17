8b607fd97434754dc6b476408311f6b99b63719c
alexsedge <35567734+alexsedge@users.noreply.github.com>
2018-02-28 21:33:38 -0500

    Add files via upload

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : AbstractAI {
+
+	int strategy;
+	Player player;
+	Logger log = new Logger("AINull");
+
+	public AIController(Player _player, int _strategy){
+		player = _player;
+		strategy = _strategy;
+		log.setSource("AIController: " + player.getName());
+	}
+}
